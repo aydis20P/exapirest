7bc7a3376813bbf26215863b6b68d494f0fc49d3
williampalacios <mich.palacios.hdz@gmail.com>
2021-06-26 23:22:46 -0500

    posgeneric t endpoint

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -8,4 +8,12 @@ class Welcome extends RestController {
 		$this->response(array('foo'=>'bar',
 							'this'=>5), 200);
 	}
+
+	public function index_post(){
+		// Takes raw data from the request
+		$json = file_get_contents('php://input');
+		// Converts it into a PHP object
+		$data = json_decode($json);
+		var_dump($data);
+	}
 }
