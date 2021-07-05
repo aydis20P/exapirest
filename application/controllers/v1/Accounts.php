dcbf1da508fe6ab32cda557eb62bd0b4e4229292
AntonioBluebird <antoniobluebird@gmail.com>
2021-07-05 12:28:53 -0500

    Se creo metodo para usar el POST

--- application/controllers/v1/Accounts.php
+++ application/controllers/v1/Accounts.php
@@ -17,7 +17,19 @@ class Accounts extends RestController {
 	}
 
 	public function index_post(){
-        echo 'hola';
+       // Takes raw data from the request
+		$json = file_get_contents('php://input');
+		// Converts it into a PHP object
+		$data = json_decode($json);
+
+		$array_query = $this->accounts_model->insert_accounts($data);
+		if (empty($array_query)){
+			$this->response("Bad request", 400);
+		}
+		else{
+			$this->response($array_query, 201);
+		}
+
 	}
 
 	public function index_put(){
--- application/models/Accounts_model.php
+++ application/models/Accounts_model.php
@@ -14,7 +14,16 @@ class Accounts_model extends CI_Model {
                     return $query->result_array();
             }
 
-            $query = $this->db->get_where('accounts', array('user_id' => $id)); //SELECT * FROM accounts WHERE 
+            $query = $this->db->get_where('accounts', array('id' => $id)); //SELECT * FROM accounts WHERE 
             return $query->row_array();
+        }
+
+        public function insert_accounts($data = FALSE){
+			if(!$data){
+				return array();
+			}
+            $this->db->insert('accounts', $data);
+            return $this->db->get_where('accounts', array('email' => $data->email))->row_array();
+
         }
 }
